8a6d2261e128627f0059adafe2e394de63220850
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-07 23:19:44 +0600

    initial commit

---
+++ src/_steps.js
@@ -0,0 +1,12 @@
+// steps to use firebase 
+// 1. create a project on console.firebase.google.com
+// 2. npm install firebase
+// 3. Register web app in firebase
+// 4. copy firebase init with config firebase projects settings
+// 5. export default firebase app 
+// 6. import app firebase init.js into your app.js
+// 7. import getAuth from firebase/auth and create auth variable
+// 8. turn on google authentication (firebase> authentication> google sign in) enable google sign in (important step)
+// 9. create google provider 
+// 10. usesigninwithpopup and pass auth and provider
+// 11. handle .then(if successful) and chatch error(if error)
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6Hx_Bwsbix60YvSra3LkbOfuslNn8-ME",
+  authDomain: "simple-firebase-authenti-55fe8.firebaseapp.com",
+  projectId: "simple-firebase-authenti-55fe8",
+  storageBucket: "simple-firebase-authenti-55fe8.appspot.com",
+  messagingSenderId: "230732014646",
+  appId: "1:230732014646:web:67856beb2b4ac7f30f6497"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
